83b03c6e7b30bb0c39589697cac5a960f1a9ad9a
Hagb Green (Guo Junyu) <hagb@hagb.name>
2023-04-15 21:22:30 +0800

    Update

--- Basic.lean
+++ Basic.lean
@@ -1,4 +1,10 @@
 import Mathlib.Data.MvPolynomial.Basic
+import Mathlib.Algebra.Hom.Embedding
+import Mathlib.Algebra.MonoidAlgebra.Support
+import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Finset.Image
+-- import Mathlib.Data.Multiset.Basic
+import Mathlib.Tactic.LibrarySearch
 
 variable {σ : Type _} {a a' a₁ a₂ : R} {n m : σ} {s : σ →₀ ℕ}
 
@@ -19,3 +25,50 @@ theorem support_zero_iff (p: MvPolynomial σ R): p = 0 ↔ p.support = ∅ :=
     rw [as_sum p, hps]
     exact rfl
 #align mv_polynomial.support_zero_iff MvPolynomial.support_zero_iff
+
+@[simp]
+theorem support_smul_eq [IsDomain R] {a : R} (ha: a≠0) {f: MvPolynomial σ R}:
+    (a • f).support = f.support := Finsupp.support_smul_eq ha
+
+
+@[simp]
+theorem support_mul_monomial [IsDomain R] (ha: a ≠ 0):
+    (p * (monomial s a)).support = p.support.map (addRightEmbedding s) := by
+  let key (q) := ∃ (s': σ→₀ℕ) (a': R), monomial s' a' = q ∧
+          (a' ≠ 0 → (p * (monomial s' a')).support = p.support.map (addRightEmbedding s'))
+  have hkey: key (monomial s a) := by
+    apply induction_on_monomial
+    ·
+      intros a'
+      use 0, a'
+      -- simp?
+      simp only [monomial_zero', ne_eq, true_and]
+      intro ha
+      unfold addRightEmbedding
+      rw [mul_comm, C_mul', support_smul_eq ha, Finset.map_eq_image]
+      -- simp?
+      simp only [add_zero, Function.Embedding.coeFn_mk, Finset.image_id']
+    ·
+      intros q n hq
+      rcases hq with ⟨s', a', hq, key'⟩
+      use s' + (Finsupp.single n 1), a'
+      constructor
+      ·rw [←hq, monomial_add_single, pow_one]
+      ·
+        intro ha'
+        specialize key' ha'
+        rw [monomial_add_single, pow_one, ←mul_assoc, support_mul_X, key']
+        unfold addRightEmbedding
+        rw [Finset.map_eq_image, Finset.map_eq_image, Finset.map_eq_image,
+            Finset.image_image]
+        -- simp?
+        simp only [Function.Embedding.coeFn_mk, comp_add_right]
+  rcases hkey with ⟨s', a', hkey', hkey⟩
+  -- simp? [monomial_eq_monomial_iff, ha] at hkey'
+  simp only [monomial_eq_monomial_iff, ha, and_false, or_false] at hkey'
+  exact (hkey'.1 ▸ hkey'.2 ▸ hkey) ha
+
+@[simp]
+theorem support_monomial_mul [IsDomain R] (ha: a ≠ 0):
+    ((monomial s a) * p).support = p.support.map (addRightEmbedding s) :=
+    mul_comm p (monomial s a) ▸ support_mul_monomial ha
--- TermOrder.lean
+++ TermOrder.lean
@@ -3,82 +3,130 @@ import Mathlib.Logic.Equiv.Defs
 import Mathlib.Logic.Nontrivial
 import Mathlib.Order.Basic
 import Mathlib.Data.Finsupp.Basic
+import Mathlib.Data.MvPolynomial.CommRing
+
+open BigOperators
+open Classical
+namespace MvPolynomial
 
 -- refer to https://github.com/leanprover-community/mathlib4/blob/master/Mathlib/Order/Synonym.lean
 
 universe u
--- variable {σ: Type u}
 variable {α : Type u}
 
 /-- A type synonym to equip a type with its term_ordericographic order. -/
 def TermOrder (α : Type u) :=
   α
-#align term_order TermOrder
 
 /-- `toTermOrder` is the identity function to the `TermOrder` of a type.  -/
 @[match_pattern]
 def toTermOrder : α ≃ TermOrder α :=
   Equiv.refl _
-#align to_term_order toTermOrder
 
 /-- `ofTermOrder` is the identity function from the `TermOrder` of a type.  -/
 @[match_pattern]
 def ofTermOrder : TermOrder α ≃ α :=
   Equiv.refl _
-#align of_term_order ofTermOrder
 
 @[simp]
 theorem toTermOrder_symm_eq : (@toTermOrder α).symm = ofTermOrder :=
   rfl
-#align to_term_order_symm_eq toTermOrder_symm_eq
 
 @[simp]
 theorem ofTermOrder_symm_eq : (@ofTermOrder α).symm = toTermOrder :=
   rfl
-#align of_term_order_symm_eq ofTermOrder_symm_eq
 
 @[simp]
 theorem toTermOrder_ofTermOrder (a : TermOrder α) : toTermOrder (ofTermOrder a) = a :=
   rfl
-#align to_term_order_of_term_order toTermOrder_ofTermOrder
 
 @[simp]
 theorem ofTermOrder_toTermOrder (a : α) : ofTermOrder (toTermOrder a) = a :=
   rfl
-#align of_term_order_to_term_order ofTermOrder_toTermOrder
 
 -- Porting note:
 -- removed @[simp] since this already follows by `simp only [EmbeddingLike.apply_eq_iff_eq]`
 theorem toTermOrder_inj {a b : α} : toTermOrder a = toTermOrder b ↔ a = b :=
   Iff.rfl
-#align to_term_order_inj toTermOrder_inj
 
 -- Porting note:
 -- removed @[simp] since this already follows by `simp only [EmbeddingLike.apply_eq_iff_eq]`
 theorem ofTermOrder_inj {a b : TermOrder α} : ofTermOrder a = ofTermOrder b ↔ a = b :=
   Iff.rfl
-#align of_term_order_inj ofTermOrder_inj
 
 namespace TermOrder
-variable [α_add: Add α] [α_zero: Zero α]
+variable [α_add: Add α] [α_zero: Zero α] [α_add_cancel_comm_monoid: AddCancelCommMonoid α]
+
 /-- A recursor for `TermOrder`. Use as `induction x using TermOrder.rec`. -/
 protected def rec {β : TermOrder α → Sort _} (h : ∀ a, β (toTermOrder a)) : ∀ a, β a := fun a => h (ofTermOrder a)
-#align term_order.rec TermOrder.rec
+-- #align term_order.rec TermOrder.rec
 
 instance add: Add (TermOrder α) := α_add
 instance zero: Zero (TermOrder α) := α_zero
-
-variable {σ : Type _}
-variable [le': LE (TermOrder (σ →₀ ℕ))]
-
--- instance lt: LE (TermOrder (σ →₀ ℕ)) := le'
-
-variable (a: TermOrder (σ →₀ ℕ)) (b: TermOrder (σ →₀ ℕ))
-
-
-class TermOrderClass (σ : Type _) [Finite σ] extends LinearOrder (TermOrder (σ →₀ ℕ)) where
-  Additive : ∀ v v₁ v₂ : TermOrder (σ →₀ ℕ), v₁ ≤ v₂ → v + v₁ ≤ v + v₂
-  zero_le : ∀ v : TermOrder (σ →₀ ℕ), 0 ≤ v
--- #align mv_polynomial.term_order MvPolynomial.TermOrder
+instance toAddCancelCommMonoid: AddCancelCommMonoid (TermOrder α) := α_add_cancel_comm_monoid
+instance toAddCommMonoid: AddCommMonoid (TermOrder α) := α_add_cancel_comm_monoid.toAddCommMonoid
 
 end TermOrder
+
+variable {α : Type _} {σ: Type _}
+-- variable {s : σ →₀ ℕ}
+-- variable {k : Type _}
+
+-- from https://leanprover.zulipchat.com/#narrow/stream/113489-new-members/topic/.E2.9C.94.20Override.20default.20ordering.20instance/near/339882298
+class ZeroLEClass (α: Type _) [Zero α] [LE α] where
+  zero_le (a: α): 0 ≤ a
+
+instance ZeroLEClass.toOrderBot [Zero α] [LE α] [ZeroLEClass α]: OrderBot α :=
+  {
+    bot := 0
+    bot_le := by
+      intro a
+      apply zero_le
+  }
+
+class TermOrderClass (α : Type _) [AddCancelCommMonoid α]
+  extends LinearOrder α,
+          ZeroLEClass α,
+          IsWellOrder α (·<·),
+          CovariantClass α α (·+·) (·≤·)
+
+variable [AddCancelCommMonoid α]
+variable [term_order_class: TermOrderClass α]
+
+-- instance TermOrder.toOrderBot: OrderBot α := 
+
+instance:
+  CovariantClass α α (·+·) (·≤·) :=
+                                      TermOrderClass.toCovariantClass
+
+instance:
+  ContravariantClass α α (·+·) (·<·) :=
+    contravariant_add_lt_of_covariant_add_le α
+
+instance:
+  CovariantClass α α (·+·) (·<·) :=
+    @AddLeftCancelSemigroup.covariant_add_lt_of_covariant_add_le α _ _ TermOrderClass.toCovariantClass
+
+instance: LinearOrderedAddCommMonoid α := {
+  add_le_add_left := by
+    intros a b hab c
+    simp only [add_le_add_iff_left, hab]
+}
+
+instance TermOrder.isWellFoundedRelation: WellFoundedRelation α :=
+  term_order_class.toWellFoundedRelation
+
+variable [TermOrderClass (TermOrder (σ→₀ℕ))]
+
+lemma TermOrder.le_of_finsupp_le {k₁ k₂: σ→₀ℕ} (h: k₁≤k₂): LE.le (α:=TermOrder (σ→₀ℕ)) k₁ k₂ := by
+  -- have key: k₁+(k₂-k₁)=k₂ := by
+  --   -- library_search
+  rw [←Iff.mpr add_tsub_cancel_iff_le h]
+  -- simp?
+  simp only [ge_iff_le, le_add_iff_nonneg_right]
+  exact ZeroLEClass.zero_le _
+
+lemma TermOrder.lt_of_finsupp_lt {k₁ k₂: σ→₀ℕ} (h: k₁<k₂): LT.lt (α:=TermOrder (σ→₀ℕ)) k₁ k₂
+  := lt_of_le_of_ne (TermOrder.le_of_finsupp_le (le_of_lt h)) (ne_of_lt (α:=σ→₀ℕ) h)
+
+end MvPolynomial
