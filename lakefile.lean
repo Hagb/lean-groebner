ef949764751d1ec770b15a3764867f1012e22230
Hagb <hagb@hagb.name>
2023-05-28 11:42:30 +0800

    Replace the soft link to regular file

--- lakefile.lean
+++ lakefile.lean
@@ -1,1 +1,39 @@
-../lean-trying-multivariable/lakefile.lean
+import Lake
+open Lake DSL
+
+package «lean-trying-multivariable» {
+  -- add any package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+lean_lib «Ideal» {
+  -- add any library configuration options here
+}
+
+lean_lib «TermOrder» {
+  -- add any library configuration options here
+}
+
+lean_lib «Multideg» {
+  -- add any library configuration options here
+}
+
+@[default_target]
+lean_lib «Division» {
+  -- add any library configuration options here
+}
+
+@[default_target]
+lean_lib «Groebner» {
+  -- add any library configuration options here
+}
+
+lean_lib «Basic» {
+  -- add any library configuration options here
+}
+
+lean_lib «Set» {
+  -- add any library configuration options here
+}
